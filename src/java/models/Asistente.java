4b6446a05b529a1e889da7554c8930a31fcd522f
tahtty <aahtty@espol.edu.ec>
2017-01-16 03:03:54 -0500

    Asistente modificado, falta json

--- src/java/models/Asistente.java
+++ src/java/models/Asistente.java
@@ -5,15 +5,29 @@
  */
 package models;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author CltControl
  */
 public class Asistente {
+    public Connection conn=null;
+    public Statement st=null;
+    public PreparedStatement ps=null;
+    public ResultSet rs=null;
     private int id;
     private String cedula;
     private String nombre;
-    private Conferencia conferencia;
+    private String apellido;
+    private int conferencia;
     private String correo;
 
     public int getId() {
@@ -40,11 +54,19 @@ public class Asistente {
         this.nombre = nombre;
     }
 
-    public Conferencia getConferencia() {
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getConferencia() {
         return conferencia;
     }
 
-    public void setConferencia(Conferencia conferencia) {
+    public void setConferencia(int conferencia) {
         this.conferencia = conferencia;
     }
 
@@ -56,12 +78,35 @@ public class Asistente {
         this.correo = correo;
     }
 
-    public Asistente(int id, String cedula, String nombre, Conferencia conferencia, String correo) {
+    public Asistente(int id, String cedula, String nombre,String apellido, int conferencia, String correo) {
         this.id = id;
         this.cedula = cedula;
         this.nombre = nombre;
         this.conferencia = conferencia;
         this.correo = correo;
+    }
+    public Asistente(){
+        
+    }
+    
+    public ArrayList<Asistente> getAsistente(){
+        ArrayList<Asistente> lista = new ArrayList();
+        try {
+            conn=Conexion.obtener();
+            st=conn.createStatement();
+            rs=st.executeQuery("select * from asistente");
+            while(rs.next()){
+                
+            lista.add(new Asistente(rs.getInt(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getInt(5),rs.getString(6)));
+            }
+            return lista;
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(Conferencia.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Conferencia.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return lista;
     }
     
     
